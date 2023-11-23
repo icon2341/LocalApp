0924fbedaff0b6c08f9c35397eed55e62ff0afa9
icon2341 <asharrahman2002@gmail.com>
2023-11-23 01:54:54 -0500

    basic map

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,68 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import {StatusBar} from "expo-status-bar";
-import Mapbox from '@rnmapbox/maps';
-import React, {Component, useEffect, useState} from "react";
-import MapboxGL from "@rnmapbox/maps";
-import { UserLocation } from '@rnmapbox/maps';
 import * as Location from 'expo-location';
-
-
+import {LocationAccuracy} from 'expo-location';
+import Mapbox from '@rnmapbox/maps';
+import MapboxGL from '@rnmapbox/maps';
+import React, {useEffect, useState} from "react";
+Mapbox.setAccessToken('pk.eyJ1IjoiaWNvbjIzNDEiLCJhIjoiY2xwM3o2ZmVjMTF2MzJxbWo2aHRtMzA4NiJ9.s2Gs6pMoeWcwVPDj0kOHgg').then(
+    () => {
+        console.log('Mapbox GL ready to go!');
+        MapboxGL.requestAndroidLocationPermissions().then(r => console.log(r));
+    }
+)
 export default function Page() {
-    const [GLReady, setGLReady] = useState(false);
 
-    const [location, setLocation] = useState< any>(null);
+    const [location, setLocation] = useState<any>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+
+    MapboxGL.setConnected(true);
+
     useEffect(() => {
         (async () => {
-            const { status } = await Location.requestForegroundPermissionsAsync();
 
+            let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
-                console.error('Permission to access location was denied');
-
+                setErrorMsg("Permission to access location was denied");
                 return;
             }
 
-            const currentLocation = await Location.getCurrentPositionAsync({});
-
-            setLocation(currentLocation.coords);
+            await Location.watchPositionAsync({accuracy: LocationAccuracy.Highest},
+                (r) => {console.log("LOCATION: "+ r.coords.longitude); setLocation(r.coords)});
         })();
     }, []);
 
-    MapboxGL.setWellKnownTileServer('Mapbox');
-    MapboxGL.setConnected(true);
-    Mapbox.setAccessToken('pk.eyJ1IjoiaWNvbjIzNDEiLCJhIjoiY2xwM3o2ZmVjMTF2MzJxbWo2aHRtMzA4NiJ9.s2Gs6pMoeWcwVPDj0kOHgg').then(
-        () => {
-            console.log('Mapbox GL ready to go!');
-            setGLReady(true);
-        }
-    )
-    if(!GLReady) return null;
     return (
         <SafeAreaProvider>
-            <Mapbox.MapView style={styles.map}/>
+            <View style={styles.page}>
+                <View style={styles.container}>
+                    <MapboxGL.MapView style={styles.map} >
+                        <MapboxGL.Camera
+                            zoomLevel={15}
+                            followUserLocation={true}
+                        />
+                        <MapboxGL.UserLocation minDisplacement={2} visible={true} animated={true}/>
+                    </MapboxGL.MapView>
+                </View>
+            </View>
         </SafeAreaProvider>
     );
 }
 
 const styles = StyleSheet.create({
     page: {
-        flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#F5FCFF"
+        backgroundColor: "#F5FCFF",
+        flex: 1
     },
     container: {
-        height: 300,
-        width: 300,
+        height: "100%",
+        width: "100%",
         backgroundColor: "tomato"
     },
     map: {
         flex: 1
     }
 });
-
